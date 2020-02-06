3230a8ea19de0f7fd07af746da72acdf6c1f4d17
Revan Jafar <revanjafar@gmail.com>
2020-02-05 16:13:05 -0800

    Cleanup and comments

--- algorithms/fib/memoization_fibonacci.py
+++ algorithms/fib/memoization_fibonacci.py
@@ -1,27 +1,24 @@
 # Implementation of the Fibonacci sequence using memoization, prints the sequence up to nth digit where n>0
-# Input: n = integer
-# Output: prints Fibonacci sequence up to n
+# Input: int n
+# Output: prints Fibonacci sequence up to n in list
 # Time Complexity: O(n)
 
 def fib(n):
-    memo = [None]*(n+1) # init memo list
-    for i in range(n+1):
-        result =  memoize(i, memo)
-        print(result)
-    return memo
-
+    memo = [None]*(n+1) # init memo list to hold computed fib
+    memoize(n, memo) # call helper func
+    print(memo)
+    return memo # return populated list at the end
 
 def memoize(n, memo):
-    if n <= 0: 
+    if n <= 0: # base case
         memo[n] = 0
-        return memo[n]
-    elif n == 1: 
+    elif n == 1: # base case
         memo[n] = 1
-        return memo[n]
-    elif memo[n] != None: return memo[n]
-
-    memo[n] = memoize(n-2, memo) + memoize(n-1, memo)
+    elif memo[n] != None: return memo[n] # calculated before so lookup in list
+    else: memo[n] = memoize(n-2, memo) + memoize(n-1, memo) # recursive call
+    
     return memo[n]
 
-fib(10)
+fib(10) # call the function with example n > 0
+
 
